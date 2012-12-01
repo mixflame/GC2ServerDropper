261169a1379f2d6f55b55a3801d0e5c927da1ffc
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2012-11-30 20:17:13 -0800

    heroku check

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -62,7 +62,9 @@ class MainController < ApplicationController
     require 'resolv'
     hostname = Resolv.getname(sender)
     logger.info "sender: #{hostname}"
-    raise 'unauthorized' if hostname.include?('amazonaws.com') == false
+    unless hostname.include?('amazonaws.com')
+      raise 'unauthorized'
+    end
   end
 
 end
