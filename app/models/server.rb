f1545b33ab0b421913b0e05fb0b61612629e4704
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2012-12-12 18:48:39 -0800

    reinvent old wheel

--- app/models/server.rb
+++ app/models/server.rb
@@ -14,13 +14,12 @@ class Server < ActiveRecord::Base
   def start
     logger.info "starting server #{self.host}:#{self.port}"
     # binding.pry
-    server_path = "#{Rails.root.to_s}/bin/server.coffee '#{self.host}' '#{self.port}' '#{self.name}' '#{self.password}' '#{self.private}' '#{self.buffer_replay}' 'false'"
+    server_path = "#{Rails.root.to_s}/bin/server.coffee '#{self.host}' '#{self.port}' '#{self.name}' '#{self.password}' '#{self.private}' '#{self.buffer_replay}'"
     logger.info "path: #{server_path}"
-    io = IO.popen(server_path) do
-      pid = $?.pid
-      logger.info "opened server, pid #{pid}"
-      self.update_attribute(:pid, pid)
-    end
+    io = IO.popen(server_path)
+    pid = io.pid
+    logger.info "opened server, pid #{pid}"
+    self.update_attribute(:pid, pid)
   end
 
 
